19e6308251e80f156dc8b6e0fd7ccbe4c010edde
bemug <chokinghazard.g@gmail.com>
2013-12-22 15:31:35 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Installing bemug's .vimrc.."
+
+#Symlink
+if [ -L ~/.vimrc ]
+then
+	echo "Removing old symbolic link"
+	rm -f ~/.vimrc
+#File
+elif [ -f ~/.vimrc ]
+then
+	echo ".vimrc already exists, backuping in .vimrc.bak"
+	mv ~/.vimrc ~/.vimrc.bak
+fi
+
+ln -s $(pwd)/.vimrc ~/.vimrc
+echo "Done!"
